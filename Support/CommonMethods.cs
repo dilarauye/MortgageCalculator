2dfe36bfe11d8c007135aa0b21cab5da00bed289
dilarauye <zeynepdilarauu@gmail.com>
2024-05-27 16:14:14 -0500

    Created AppSettings file

---
+++ Support/CommonMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace MortgageCalculator.Support
+{
+    internal class CommonMethods
+    {
+
+        public static string GetAppSettingsValue(string key)
+        {
+
+            return new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("ApSettings.json").Build().GetSection(key).Value;
+
+        }
+
+    }
+}
